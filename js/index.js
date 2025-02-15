801ad2502f8c20c4949defdc7670c02135978b24
Freddy~2 <freddywafo3@gmail.com>
2025-02-15 12:44:10 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
 
-let today =new Date();
+const today =new Date();
 
-let date = today.toLocaleDateString('fr-FR',{
+
+const hour = today.toLocaleTimeString('fr-FR',{
+    hour: '2-digit',
+    minute:'2-digit'
+}
+)
+const date = today.toLocaleDateString('fr-FR',{
     weekday: 'long',
     day: 'numeric',
     month: 'long',
     year: 'numeric',
 }
 );
+document.querySelector('#hour').innerHTML= hour;
 
 document.querySelector('#date').innerHTML= date;
-console.log(date);
